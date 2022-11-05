b984c3ada9e70576f28006969be031975d30955f
William Kenzie <william@kenzie.one>
2022-11-05 13:26:21 -0500

    V2 Initiation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use rocket::serde::{Deserialize, json::Json};
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+
+/* Structures for Responses */
+struct V2response<'r> {
+    status: &'r i8,
+    complete: bool
+}
+
+/* Endpoints */
+#[get("/")]
+fn index() -> &'static str {
+    "https://api.cybfi.net/v2"
+}
+
+#[get("/v2")]
+fn v2() -> &'static str {
+    Json(v2Response(200, true))
+}
+
+#[launch]
+fn main() -> _ {
+    rocket::build().mount("/", routes![index])
 }
